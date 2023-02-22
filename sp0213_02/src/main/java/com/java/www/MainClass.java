bbb6154da197b4fd5f3213744a2b6f188066cbac
KV006 <KV006@DESKTOP-T7U8C53>
2023-02-23 08:59:03 +0900

    0223 아침에

---
+++ sp0213_02/src/main/java/com/java/www/MainClass.java
@@ -0,0 +1,22 @@
+package com.java.www;
+
+import org.springframework.context.support.AbstractApplicationContext;
+import org.springframework.context.support.GenericXmlApplicationContext;
+
+public class MainClass {
+	
+	public static void main(String[] args) {
+		
+		// HelloWorldEn hello1 = new HelloWorldEn();
+		AbstractApplicationContext ctx = new GenericXmlApplicationContext("classpath:beans.xml");
+		Hello hello1 = ctx.getBean("hello1", Hello.class);
+		
+		callMethod(hello1);
+		
+	}
+	
+	public static void callMethod(Hello helloVo) {
+		helloVo.sayHello();
+	}
+	
+}
