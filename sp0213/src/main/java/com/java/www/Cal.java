32c5381f375566edf2f00944d350bbdb89c199dd
KV006 <KV006@DESKTOP-T7U8C53>
2023-02-22 18:19:13 +0900

    0222 업로드

---
+++ sp0213/src/main/java/com/java/www/Cal.java
@@ -0,0 +1,31 @@
+package com.java.www;
+
+public class Cal {
+	private int firstNum;
+	private int secondNum;
+	
+	public int getFirstNum() {
+		return firstNum;
+	}
+
+	public void setFirstNum(int firstNum) {
+		this.firstNum = firstNum;
+	}
+
+	public int getSecondNum() {
+		return secondNum;
+	}
+
+	public void setSecondNum(int secondNum) {
+		this.secondNum = secondNum;
+	}
+
+	public void add() {
+		System.out.println("두 수의 합 : " + (firstNum + secondNum));
+	}
+	
+	public void sub() {
+		System.out.println("두 수의 차 : " + (firstNum - secondNum));
+	}
+	
+}
---
+++ sp0213/src/main/java/com/java/www/MainClass.java
@@ -0,0 +1,12 @@
+package com.java.www;
+
+public class MainClass {
+	
+	public static void main(String[] args) {
+		Cal c = new Cal();
+		c.setFirstNum(10);
+		c.setSecondNum(5);
+		c.add();
+	}
+}
+
